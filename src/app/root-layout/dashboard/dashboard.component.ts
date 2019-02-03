fd693e94e3317c2d03e4c892ccab6bd16e6ac39d
zzanette <alysson.zanette@hotmail.com>
2019-02-03 10:52:31 -0200

    Adicionado busca de especies de plantas no dashboard

--- src/app/root-layout/dashboard/dashboard.component.ts
+++ src/app/root-layout/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EspeciePlantaService } from '../../shared/services/especie-planta/especie-planta.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,69 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-    places: Array<any> = [];
+    private plantas: Array<any> = [];
 
-    constructor() {
-        this.places = [
-            {
-                imgSrc: 'assets/images/card-1.jpg',
-                place: 'Cozy 5 Stars Apartment',
-                description:
-                    // tslint:disable-next-line:max-line-length
-                    'The place is close to Barceloneta Beach and bus stop just 2 min by walk and near to "Naviglio" where you can enjoy the main night life in Barcelona.',
-                charge: '$899/night',
-                location: 'Barcelona, Spain'
-            },
-            {
-                imgSrc: 'assets/images/card-2.jpg',
-                place: 'Office Studio',
-                description:
-                    // tslint:disable-next-line:max-line-length
-                    'The place is close to Metro Station and bus stop just 2 min by walk and near to "Naviglio" where you can enjoy the night life in London, UK.',
-                charge: '$1,119/night',
-                location: 'London, UK'
-            },
-            {
-                imgSrc: 'assets/images/card-3.jpg',
-                place: 'Beautiful Castle',
-                description:
-                    // tslint:disable-next-line:max-line-length
-                    'The place is close to Metro Station and bus stop just 2 min by walk and near to "Naviglio" where you can enjoy the main night life in Milan.',
-                charge: '$459/night',
-                location: 'Milan, Italy'
-            },
-            {
-                imgSrc: 'assets/images/card-1.jpg',
-                place: 'Cozy 5 Stars Apartment',
-                description:
-                    // tslint:disable-next-line:max-line-length
-                    'The place is close to Barceloneta Beach and bus stop just 2 min by walk and near to "Naviglio" where you can enjoy the main night life in Barcelona.',
-                charge: '$899/night',
-                location: 'Barcelona, Spain'
-            },
-            {
-                imgSrc: 'assets/images/card-2.jpg',
-                place: 'Office Studio',
-                description:
-                    // tslint:disable-next-line:max-line-length
-                    'The place is close to Metro Station and bus stop just 2 min by walk and near to "Naviglio" where you can enjoy the night life in London, UK.',
-                charge: '$1,119/night',
-                location: 'London, UK'
-            },
-            {
-                imgSrc: 'assets/images/card-3.jpg',
-                place: 'Beautiful Castle',
-                description:
-                    // tslint:disable-next-line:max-line-length
-                    'The place is close to Metro Station and bus stop just 2 min by walk and near to "Naviglio" where you can enjoy the main night life in Milan.',
-                charge: '$459/night',
-                location: 'Milan, Italy'
-            },
-        ];
-    }
-
+    constructor(private especiePlatntaService: EspeciePlantaService) { }
 
     ngOnInit() {
+        this.especiePlatntaService.getAllOnPage('0').subscribe(page => {
+            this.plantas = page.content;
+        });
     }
-
 }
