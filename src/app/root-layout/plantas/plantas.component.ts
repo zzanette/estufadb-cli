679cbeb92cd7fcdfe829a0f86903495d6128b599
zzanette <alysson.zanette@hotmail.com>
2019-02-03 15:43:42 -0200

    Ajustado stat de plantas

--- src/app/root-layout/plantas/plantas.component.ts
+++ src/app/root-layout/plantas/plantas.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
+import { PlantaService } from '../../shared/services/planta/planta.service';
 
 @Component({
-  selector: 'app-plantas',
-  templateUrl: './plantas.component.html',
-  styleUrls: ['./plantas.component.scss']
+    selector: 'app-plantas',
+    templateUrl: './plantas.component.html',
+    styleUrls: ['./plantas.component.scss']
 })
 export class PlantasComponent implements OnInit {
+    plantas: Observable<any>;
 
-  constructor() { }
+    constructor(private plantaService: PlantaService, private route: ActivatedRoute) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.plantas = this.route.paramMap.pipe(
+            switchMap(params => {
+                const id = params.get('id');
+                if (id == null) {
+                    return this.plantaService.getAllOnPage('0');
+                }
+
+                return this.plantaService.getByEspecieId(id);
+            })
+        );
+    }
 
 }
